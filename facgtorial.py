c736ccc79386f854eab269aa55e2efd25a7735d7
krupalilunagariya <113034198+krupalilunagariya@users.noreply.github.com>
2023-12-20 13:10:29 +0530

    Update facgtorial.py

--- facgtorial.py
+++ facgtorial.py
@@ -1,4 +1,4 @@
-ut("Enter a number: "))    
+num = int(input("Enter a number: "))    
 factorial = 1    
 if num < 0:    
    print(" Factorial does not exist for negative numbers")    
@@ -7,3 +7,4 @@ elif num == 0:
 else:    
    for i in range(1,num + 1):    
        factorial = factorial*i    
+   print("The factorial of",num,"is",factorial)    
